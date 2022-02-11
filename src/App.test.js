aeb31fe09893cf61abd2f793b03a19f13e166b00
lens0021 <lorentz0021@gmail.com>
2022-02-11 23:32:09 +0900

    Replace WORD MASTER with KORD MASTER

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react'
 import App from './App'
 
-test('renders the WORD MASTER title', () => {
+test('renders the KORD MASTER title', () => {
   render(<App />)
-  const title = screen.getByText(/WORD MASTER/i)
+  const title = screen.getByText(/KORD MASTER/i)
   expect(title).toBeInTheDocument()
 })
